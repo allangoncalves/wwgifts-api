4d235f1d6e2029bef533500e2ef9f7709429f4cd
Allan Gonçalves <allanvgoncalves@gmail.com>
2019-05-24 12:32:27 -0300

    read credentials

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module WwgiftsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.eager_load_paths << Rails.root.join('lib')
+    load_path_strategy = Rails.env.production? ? :eager_load_paths : :autoload_paths
+    config.public_send(load_path_strategy) << Rails.root.join('lib')
   end
 end
