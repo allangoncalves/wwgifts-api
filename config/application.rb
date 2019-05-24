f65729bc5c8964297256ae46f110fbd129d06908
Allan Gonçalves <allanvgoncalves@gmail.com>
2019-05-24 10:55:23 -0300

    read encrypted secrets

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,6 @@ module WwgiftsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.eager_load_paths << Rails.root.join('lib')
-    config.read_encrypted_secrets = true
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
